64198bed1a8d642ae30126f4e9092418a6dd5786
wai-doi <yusuke.doi@esm.co.jp>
2019-10-01 18:21:21 +0900

    三角測量終了

--- spec/lib/money_spec.rb
+++ spec/lib/money_spec.rb
@@ -8,4 +8,9 @@ describe 'Money' do
     product = five.times(3)
     expect(product.amount).to eq 15
   end
+
+  it 'test equality' do
+    expect(Dollar.new(5) == Dollar.new(5)).to be true
+    expect(Dollar.new(5) == Dollar.new(6)).to be false
+  end
 end
