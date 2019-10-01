d344bcce510a02e22fd48a0a6ee37ca06aa6dea8
wai-doi <yusuke.doi@esm.co.jp>
2019-10-01 17:48:02 +0900

    第２章 明白な実装が終了

--- spec/lib/money_spec.rb
+++ spec/lib/money_spec.rb
@@ -1,8 +1,11 @@
 describe 'Money' do
   it 'test multiplication' do
     five = Dollar.new(5)
-    five.times(2)
 
-    expect(five.amount).to eq 10
+    product = five.times(2)
+    expect(product.amount).to eq 10
+
+    product = five.times(3)
+    expect(product.amount).to eq 15
   end
 end
